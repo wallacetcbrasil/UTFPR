9965eb032854da2da2f6ebed541086d18ad655d9
wallacetcbrasil <wallacetcbrasil.e@gmail.com>
2024-10-02 15:14:58 -0300

    a

---
+++ 2 Periodo/Aula 1/Introdução a Linguagem C.c
@@ -0,0 +1,83 @@
+/*
+    Aula 1
+
+- vai ter que memorizar os codigos, pois vai ter que ir direto pro moodle
+- so prova, sem atividades
+    
+    Prova 1 30/10/24 aula 1 - 3
+    
+    Estrutura de um programa
+        - fluxograma
+
+    Tipos de dados 
+        - %c - caractere (unico)
+        - %d ou i- decimal inteiro
+        - %f - floot 
+        - %lf - double
+        - %s - string (cadeia de caracteres)
+        - %p - ponteiro
+        
+        int, str, float, char
+
+    operadores
+
+        Aritmeticos
+        - - multiplicação
+        - + adição
+        - * multiplicação
+        - / divisão
+        - % resto da divisão
+
+        Relacionais
+        - >= maior igual
+        - <= menor igual
+        - < menor igual
+        - > maior igual
+        - = atribuição
+        - == igualdade
+        - != diferente de 
+
+        Logico
+        - && AND
+        - ! NOT
+        - || OR
+
+    Declaracão e escopo de variaveis
+        - 
+
+    Procedimentos de entrada de dados via teclado (scanf) e escrita na tela (printf).
+        - scanf("%d", &var);
+        - printf("texto");
+
+    Estruturas de decisão (if, if-else, switch).
+        - if{}
+        - if{
+            Comando 1
+          } else {
+            comando 2            
+          }
+
+        - switch (variavel){
+            case1:
+                codigo;
+                break;
+            case2:
+                codigo;
+                break;
+            default:
+                comando padrão;
+
+        }
+        
+    Estruturas de repetição (while, do-while, for).
+        - while
+        - while {}do{}
+        - for {}do{}
+*/ 
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello world!");
+    return 0;
+}
